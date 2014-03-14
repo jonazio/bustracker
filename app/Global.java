9f8ced459f3a69fb54016af0d9fe6ef31253c819
Jonas Ahsin <jonas.ahsin@kentor.se>
2014-03-14 15:29:53 +0100

    Added dummy data for in-memory database

---
+++ app/Global.java
@@ -0,0 +1,33 @@
+import com.avaje.ebean.Ebean;
+import models.BusPosition;
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.libs.Yaml;
+
+import java.util.List;
+import java.util.Map;
+
+public class Global extends GlobalSettings{
+
+    public void onStart(Application app){
+        Logger.info("Application has started");
+       InitialData.insert(app);
+    }
+
+    static class InitialData {
+
+        public static void insert(Application application){
+            if (Ebean.find(BusPosition.class).findRowCount() == 0){
+
+                Map<String, List<Object>> all = (Map<String,List<Object>>) Yaml.load("initial-data.yml");
+
+                // insert dummy bus positions
+                Ebean.save(all.get("busposition"));
+
+            }
+        }
+
+    }
+
+}
--- app/controllers/JMSProducer.java
+++ app/controllers/JMSProducer.java
@@ -4,17 +4,17 @@ import javax.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class JMSProducer {
-    public void produce() {
+    public void produce(String textMessage) {
         String url = "tcp://localhost:61616";
         ConnectionFactory factory = new ActiveMQConnectionFactory(url);
         try {
             Connection connection = factory.createConnection();
             Session session = connection.createSession(false,
                     Session.AUTO_ACKNOWLEDGE);
-            Topic topic = session.createTopic("TestTopic");
+            Topic topic = session.createTopic("BusTopic");
             MessageProducer producer = session.createProducer(topic);
             TextMessage msg = session.createTextMessage();
-            msg.setText("Hello JMS World");
+            msg.setText(textMessage);
             producer.send(msg);
         }
         catch(JMSException exp) {
